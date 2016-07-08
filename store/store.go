0742e8304ec9f51ab48c691effcd9e771b448a50
George Antoniadis <george@noodles.gr>
2016-07-08 18:30:20 +0100

    Move around some stuff

--- store/store.go
+++ store/store.go
@@ -2,6 +2,24 @@ package store
 
 import "errors"
 
+// Key for use in the ClusteringKey.
+type Key interface{}
+
+// ClusteringKey is a composition of one of more Keys.
+// A ClusteringKey can be either complete or incomplete.
+// Complete ClusteringKeys can be persisted, while non-complete ones can
+// only be used for querying.
+// eg. `func (ck *ClusteringKey) IsComplete() { return len(ck.GetKeys()) == 3 }`
+type ClusteringKey interface {
+	// GetKeys returns a list of the Keys that comprise the ClusteringKey.
+	GetKeys() []Key
+	// IsComplete checks the ClusteringKey for completenes.
+	IsComplete() bool
+}
+
+// Value of the key-value pairs
+type Value interface{}
+
 // ErrClusteringKeyNotFound when a complete clustering key was provided
 // but could not be retrieved.
 var ErrClusteringKeyNotFound = errors.New("ClusteringKey not found.")
