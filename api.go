ab48625afb21f9473a73453cdedf325f5dacbdf7
George Antoniadis <george@noodles.gr>
2016-07-08 18:34:19 +0100

    Add sample rest api for instances to `POST` and `PATCH` through the `journal` and `GET` through the `repository`

---
+++ api.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"time"
+
+	"github.com/kataras/iris"
+	"github.com/nimona/go-nimona/journal"
+	"github.com/nimona/go-nimona/repository"
+	"github.com/pborman/uuid"
+)
+
+type API struct {
+	ownerID   string
+	journal   *journal.Journal
+	instances *repository.InstanceRepository
+}
+
+func NewAPI(ownerID string, journal *journal.Journal, instanceRepository *repository.InstanceRepository) *API {
+	return &API{
+		ownerID:   ownerID,
+		journal:   journal,
+		instances: instanceRepository,
+	}
+}
+
+func (api *API) GetInstance(c *iris.Context) {
+	id := c.Param("id")
+	instance, err := api.instances.GetResourceByID(id)
+	if err != nil {
+		c.JSON(iris.StatusNotFound, iris.Map{"error": "Not found"})
+		return
+	}
+	c.JSON(iris.StatusOK, instance)
+}
+
+type InstancePostRequest struct {
+	ID      string      `json:"id"`
+	Type    string      `json:"type"`
+	Payload interface{} `json:"payload"`
+}
+
+func (api *API) PostInstance(c *iris.Context) {
+	instanceRequest := &InstancePostRequest{}
+	err := c.ReadJSON(&instanceRequest)
+	if err != nil {
+		c.JSON(iris.StatusBadRequest, iris.Map{"error": "Invalid JSON"})
+		return
+	}
+
+	id := uuid.New()
+	instanceCreated := &repository.Event{
+		Topic: "Created",
+		Payload: &repository.InstanceCreatedEvent{
+			ID:      id,
+			Type:    instanceRequest.Type,
+			OwnerID: api.ownerID,
+			Created: time.Now(),
+			Updated: time.Now(),
+			Payload: instanceRequest.Payload,
+		},
+	}
+
+	err = api.journal.AppendPayload(instanceCreated)
+	if err != nil {
+		c.JSON(iris.StatusInternalServerError, iris.Map{"error": err})
+		return
+	}
+	instance, err := api.instances.GetResourceByID(id)
+	if err != nil {
+		c.JSON(iris.StatusInternalServerError, iris.Map{"error": err})
+		return
+	}
+	c.JSON(iris.StatusOK, instance)
+}
+
+type InstancePatchRequest struct {
+	Payload interface{} `json:"payload"`
+}
+
+func (api *API) PatchInstance(c *iris.Context) {
+	instanceRequest := &InstancePatchRequest{}
+	err := c.ReadJSON(&instanceRequest)
+	if err != nil {
+		c.JSON(iris.StatusBadRequest, iris.Map{"error": "Invalid JSON"})
+		return
+	}
+
+	id := c.Param("id")
+	instanceUpdated := &repository.Event{
+		Topic: "Updated",
+		Payload: &repository.InstanceUpdatedEvent{
+			ID:      id,
+			Updated: time.Now(),
+			Payload: instanceRequest.Payload,
+		},
+	}
+
+	err = api.journal.AppendPayload(instanceUpdated)
+	if err != nil {
+		c.JSON(iris.StatusInternalServerError, iris.Map{"error": err})
+		return
+	}
+	instance, err := api.instances.GetResourceByID(id)
+	if err != nil {
+		c.JSON(iris.StatusInternalServerError, iris.Map{"error": err})
+		return
+	}
+	c.JSON(iris.StatusOK, instance)
+}
